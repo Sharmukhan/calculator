b8d9e00f9896b20a128d8ab9aff1436276f398e3
Sharmukhan <sharmukhan@gmail.com>
2024-12-24 18:37:52 +0500

    Initial Commit

---
+++ kolkulyatorTests/kolkulyatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  kolkulyatorTests.swift
+//  kolkulyatorTests
+//
+//  Created by Kymbat Sharmukhan on 24.12.2024.
+//
+
+import Testing
+@testable import kolkulyator
+
+struct kolkulyatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
